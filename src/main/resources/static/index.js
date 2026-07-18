bb01d56e08365bacc0f4f516119288ed480f72d1
KOTHAGUNDLA SYAMALA <ksyamala125@gmail.com>
2026-07-18 12:08:39 +0530

    toggle feature added

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,34 @@
 // alert("hello from js")
 console.log("hello from js")
+var users = [
+   { "name":"syamala",
+    "gender":"Female",
+    "img":"jane.png"
+
+       }    ,
+{
+    "name":"john",
+    "gender":"Male",
+    "img":"john.png"
+
+}
+]
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
